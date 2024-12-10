e9cb811d38a4590612a29c16304f0408929d8b54
Drtikolnik <drtluk00@gasos-ro.cz>
2024-12-10 07:32:43 +0100

    ahmed

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        for (int i=1; i<=30; i++) {
+            System.out.println("Ahoj nadsvěte!");
+        }
+
+        System.out.println("NAPIŠ SLOVO");
+        String slovo1 = sc.nextLine();
+        System.out.println(slovo1);
+
+
+
+        System.out.println("NAPIŠ ČÍSLO");
+        String cislo1 = sc.nextLine();
+        System.out.println(cislo1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
